5191c00f1787aad42741d82f87c59ec8f374dba6
Osama Halabi <ohalabi@gmail.com>
2020-08-27 10:16:28 +0300

    Unit 2: Lecture 6 constructors and locale

---
+++ Unit02/ClothingItem/src/App.java
@@ -0,0 +1,43 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args) {
+        ClothingItem item = new ClothingItem("Shirt", "M", 199.99, 200); // call the default constructor
+//        item.setType("Shirt");
+//        item.setSize("M");
+//        item.setPrice(199.99);
+//        item.setQuantity(5);
+
+        System.out.println(item.getType());
+        System.out.println(item.getSize());
+        System.out.println(item.getPrice());
+        System.out.println(item.getQuantity());
+
+        // calling constructor #1
+        var item2 = new ClothingItem();
+        System.out.println(item2.getType());
+        System.out.println(item2.getSize());
+        System.out.println(item2.getPrice());
+        System.out.println(item2.getQuantity());
+
+        //calling constructor #3 with one parameter
+        var item3 = new ClothingItem("Pants");
+        System.out.println(item3.getType());
+        System.out.println(item3.getSize());
+        System.out.println(item3.getPrice());
+        System.out.println(item3.getQuantity());
+
+
+        // Formatted printing
+        var totalPrice = item.getPrice() * item.getQuantity();
+
+        //var locale = Locale.UK;
+        var locale = new Locale("ar", "QA");
+        var numberFormatter = NumberFormat.getNumberInstance(locale);
+        var currencyForamtter = NumberFormat.getCurrencyInstance(locale);
+        var output = String.format("Your %S order will cost %s",
+                item.getType(), currencyForamtter.format(totalPrice));
+        System.out.println(output);
+    }
+}
---
+++ Unit02/ClothingItem/src/ClothingItem.java
@@ -0,0 +1,57 @@
+public class ClothingItem {
+    private String type;
+    private String size;
+    private double price;
+    private int quantity;
+
+    //Constructors
+
+    //constructor #1
+    public ClothingItem (){
+    }
+
+    // constructor #2
+    public ClothingItem(String type, String size, double price, int quantity) {
+        this.type = type;
+        this.size = size;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // contstructor #3
+    public ClothingItem(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
