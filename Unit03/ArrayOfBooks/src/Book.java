f87bb32e681696223684270e16d1e5026d2e35e8
Osama Halabi <ohalabi@gmail.com>
2020-09-03 12:48:45 +0300

    Unit 3: Lecture 9 Array of objects

---
+++ Unit03/ArrayOfBooks/src/Book.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Book {
+    private String bookTitle;
+
+    public Book(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public static void main(String[] args) {
+        Book[] myLibrary = new Book[3]; // array of objects
+
+        Book a = new Book("Java Programming");
+        myLibrary[0] = a;
+
+        Book b = new Book("Python Programming");
+        myLibrary[1] = b;
+
+        myLibrary[2] = new Book("C++ Programming");
+
+        for (var temp: myLibrary) {
+            System.out.println(temp.getBookTitle());
+        }
+    }
+}
